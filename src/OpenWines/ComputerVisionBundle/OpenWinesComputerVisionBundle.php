a90704491f312f0e96f10313aa05fea1605f4dcc
Ronan Guilloux <ronan.guilloux@gmail.com>
2017-02-20 00:09:56 +0100

    quick fix

--- src/OpenWines/ComputerVisionBundle/OpenWinesComputerVisionBundle.php
+++ src/OpenWines/ComputerVisionBundle/OpenWinesComputerVisionBundle.php
@@ -2,8 +2,16 @@
 
 namespace OpenWines\ComputerVisionBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class OpenWinesComputerVisionBundle extends Bundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    }
 }
